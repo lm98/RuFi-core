105f311cecaad85ff06aac15ad57a0c20cd95466
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-08 17:46:25 +0100

    test: add the gradient integration test

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -131,3 +131,29 @@ pub fn fully_connected_topology_map(elems: Vec<i32>) -> HashMap<i32, Vec<i32>> {
         .map(|elem| (elem, new_elems.clone()))
         .collect()
 }
+
+#[derive(Debug, Clone)]
+pub struct DeviceState {
+    pub self_id: i32,
+    pub exports: HashMap<i32, Export>,
+    pub local_sensor: HashMap<SensorId, Rc<Box<dyn Any>>>,
+    pub nbr_sensor: HashMap<SensorId, HashMap<i32, Rc<Box<dyn Any>>>>,
+}
+
+impl DeviceState {
+    pub fn update_exports(&mut self, nbr: i32, export: Export) {
+        self.exports.insert(nbr, export);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Topology {
+    pub devices: Vec<i32>,
+    pub states: HashMap<i32, DeviceState>,
+}
+
+impl Topology {
+    pub fn new(devices: Vec<i32>, states: HashMap<i32, DeviceState>) -> Self {
+        Topology { devices, states }
+    }
+}
